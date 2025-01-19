9f30bcd926766b559f1d7e960399b3088ced79b6
Manik2375 <manikkpr2375@gmail.com>
2025-01-19 21:57:53 +0530

    Add Cards List

--- src/components/Benefits.jsx
+++ src/components/Benefits.jsx
@@ -1,15 +1,22 @@
+import { benefits } from "../constants";
 import { Carousel, IconButton } from "@material-tailwind/react";
 import Heading from "./Heading";
 import Section from "./Section";
+import Arrow from "../assets/svg/Arrow";
+import { GradientLight } from "./design/Benefits";
+import ClipPath from "../assets/svg/ClipPath";
 
 const Benefits = () => {
   return (
     <Section id="events">
       <div className="container relative z-2">
         <Heading className="md:max-w-md lg:max-w-2xl" title="Events" />
-        <div className="image-carousel-container h-svh" style={{
-          "maxHeight": "32.5em"
-        }}>
+        <div
+          className="image-carousel-container h-svh mb-20"
+          style={{
+            maxHeight: "32.5em",
+          }}
+        >
           <Carousel
             className="rounded-xl"
             prevArrow={({ handlePrev }) => (
@@ -89,7 +96,7 @@ const Benefits = () => {
               alt="image 3"
               className="h-full w-full object-cover"
             />
-              <img
+            <img
               src="https://images.unsplash.com/photo-1497436072909-60f360e1d4b1?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2560&q=80"
               alt="image 1"
               className="h-full w-full object-cover"
@@ -105,6 +112,51 @@ const Benefits = () => {
               className="h-full w-full object-cover"
             />
           </Carousel>
+        </div>
+        <div className="flex flex-wrap gap-10 mb-10">
+          {benefits.map((item) => (
+            <div
+              className="block relative p-0.5 bg-no-repeat bg-[length:100%_100%] md:max-w-[24rem]"
+              style={{
+                backgroundImage: `url(${item.backgroundUrl})`,
+              }}
+              key={item.id}
+            >
+              <div className="relative z-2 flex flex-col min-h-[22rem] p-[2.4rem] pointer-events-none">
+                <h5 className="h5 mb-5">{item.title}</h5>
+                <p className="body-2 mb-6 text-n-3">{item.text}</p>
+                <div className="flex items-center mt-auto">
+                  <img
+                    src={item.iconUrl}
+                    width={48}
+                    height={48}
+                    alt={item.title}
+                  />
+                  <p className="ml-auto font-code text-xs font-bold text-n-1 uppercase tracking-wider">
+                    Explore more
+                  </p>
+                  <Arrow />
+                </div>
+              </div>
+              {item.light && <GradientLight />}
+              <div
+                className="absolute inset-0.5 bg-n-8"
+                style={{ clipPath: "url(#benefits)" }}
+              />
+              <div className="absolute inset-0 opacity-0 transition-opacity hover:opacity-10">
+                {item.imageUrl && (
+                  <img
+                    src={item.imageUrl}
+                    width={380}
+                    height={362}
+                    alt={item.title}
+                    className="w-full h-full object-cover"
+                  />
+                )}
+              </div>
+              <ClipPath />
+            </div>
+          ))}
         </div>
       </div>
     </Section>
