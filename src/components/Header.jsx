8ecd361ea84c3b5b587fa3ceb8285efa17da340d
Rushil <workwithrushil@gmail.com>
2025-01-22 22:33:25 +0530

    added-murious-logo-in-navbar

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,6 @@
 import { useLocation } from "react-router-dom";
 import { disablePageScroll, enablePageScroll } from "scroll-lock";
-
-import { brainwave } from "../assets";
+import muriousLogo from "../assets/muriousLogo.webp"
 import { navigation } from "../constants";
 import Button from "./Button";
 import MenuSvg from "../assets/svg/MenuSvg";
@@ -36,8 +35,8 @@ const Header = () => {
       }`}
     >
       <div className="flex items-center px-5 lg:px-7.5 xl:px-10 max-lg:py-4">
-        <a className="block w-[12rem] xl:mr-8" href="#hero">
-          <img src={brainwave} width={190} height={40} alt="" />
+        <a className="block w-[12rem] xl:mr-8" href="/">
+          <img src={muriousLogo} width={50} height={25} alt="logo" />
         </a>
 
         <nav
